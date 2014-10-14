a43461bdb4272c45fd9093c942a3aa50985a731a
Paul <Paul@10.50.137.11>
2014-10-14 19:39:59 +0200

    pula

--- src/java/entity/Timetable.java
+++ src/java/entity/Timetable.java
@@ -6,6 +6,8 @@
 
 package entity;
 
+import java.util.Date;
+
 /**
  *
  * @author Paul
@@ -14,12 +16,75 @@ public class Timetable {
     
     private int id;
     private double price;
-    private String arrivalDate,departureDate;
+    private Date  arrivalDate,departureDate;
     private Airport arrivalAirport,departureAirport;
     private Plane plane;
     
-    public Timetable(int id, String arrivalDate, String departureDate, float price, 
+    public Timetable(int id, Date arrivalDate, Date departureDate, float price, 
             Airport arrivalAirport,Airport departureAirport,Plane plane) {
-        
+        this.id = id;
+        this.arrivalDate = arrivalDate;
+        this.departureDate = departureDate;
+        this.price = price;
+        this.arrivalAirport = arrivalAirport;
+        this.departureAirport = departureAirport;
+        this.plane = plane;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Date getArrivalDate() {
+        return arrivalDate;
+    }
+
+    public void setArrivalDate(Date arrivalDate) {
+        this.arrivalDate = arrivalDate;
+    }
+
+    public Date getDepartureDate() {
+        return departureDate;
+    }
+
+    public void setDepartureDate(Date departureDate) {
+        this.departureDate = departureDate;
+    }
+
+    public Airport getArrivalAirport() {
+        return arrivalAirport;
+    }
+
+    public void setArrivalAirport(Airport arrivalAirport) {
+        this.arrivalAirport = arrivalAirport;
+    }
+
+    public Airport getDepartureAirport() {
+        return departureAirport;
+    }
+
+    public void setDepartureAirport(Airport departureAirport) {
+        this.departureAirport = departureAirport;
+    }
+
+    public Plane getPlane() {
+        return plane;
+    }
+
+    public void setPlane(Plane plane) {
+        this.plane = plane;
+    }
+    
 }
